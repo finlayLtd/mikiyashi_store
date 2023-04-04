d308095d167cad18acd78cf07b84bc6102055b4a
SuperSmile424 <happysmilegrow@gmail.com>
2023-04-05 04:13:54 +0900

    go backend

---
+++ README.md
@@ -0,0 +1,37 @@
+# nuxt-golang-sample ![license](https://img.shields.io/github/license/nagaokayuji/nuxt-golang) ![size](https://img.shields.io/github/languages/code-size/nagaokayuji/nuxt-golang) ![forks](https://img.shields.io/github/forks/nagaokayuji/nuxt-golang) 
+
+RESTful API server and SPA.
+
+## About
+
+- [Docker](https://www.docker.com/) as the container service
+- [MySQL](https://www.mysql.com/) 5.7 as the database
+- [Golang](https://golang.org/) 1.16.0 as the RESTFul API server
+- [Node.js](https://nodejs.org/en/) 15.11.0 as the run-time environment to run JavaScript
+
+## Running the app in local development
+
+- build
+
+```sh
+$ docker-compose build
+```
+
+- run app
+
+Simply run:
+
+```sh
+$ docker-compose run
+```
+
+By default you can access server via http://localhost:3000
+
+# Contributing
+
+Your contributions are always welcome!
+Feel free to raise Issues, or send Pull Requests.
+
+# License
+
+MIT License.
---
+++ golang/controllers/todoController.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"time"
+
+	"golang/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+// 全件取得
+func GetAllTodos(c *gin.Context) {
+	var todos []models.Todo
+	models.DB.Order("created_at desc").Find(&todos)
+	c.JSON(http.StatusOK, todos)
+}
+
+// UUIDから1件取得
+func GetTodo(c *gin.Context) {
+	uuid := c.Params.ByName("uuid")
+	var todo models.Todo
+	if err := models.DB.Where("uuid = ?", uuid).First(&todo).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+// 1件登録
+func CreateTodo(c *gin.Context) {
+	var input models.CreateTodoInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err.Error())
+		return
+	}
+
+	todo := models.Todo{UUID: uuid.New().String(), Title: input.Title, State: false, Deadline: input.Deadline, CreatedAt: time.Now(), UpdatedAt: time.Now()}
+	models.DB.Create(&todo)
+	c.JSON(http.StatusOK, todo)
+}
+
+func ToggleStatus(c *gin.Context) {
+	uuid := c.Params.ByName("uuid")
+	var todo models.Todo
+	if err := models.DB.Where("uuid = ?", uuid).First(&todo).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+		return
+	}
+	todo.State = !todo.State
+	models.DB.Save(&todo)
+	c.JSON(http.StatusOK, todo)
+}
+
+func DeleteTodo(c *gin.Context) {
+	uuid := c.Params.ByName("uuid")
+	if row := models.DB.Delete(&models.Todo{}, "uuid = ?", uuid).RowsAffected; row != 1 {
+		c.AbortWithStatus(404)
+		return
+	}
+	c.Status(http.StatusOK)
+}
